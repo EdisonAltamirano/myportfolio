bc37710273abd1bfbf086fa334139080ebc75d50
EdisonAltamirano <edisonaltamirano2000@outlook.com>
2025-06-15 00:07:53 -0500

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -8,6 +10,7 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/myportfolio' : '',
 }
 
 module.exports = nextConfig 
